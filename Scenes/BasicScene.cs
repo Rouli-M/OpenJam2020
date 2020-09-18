7711b149c7a6a687851a2b34205b36dfbce11d64
Efflam <efflam-fifi@hotmail.fr>
2020-09-18 21:20:14 +0200

    Add player

--- Scenes/BasicScene.cs
+++ Scenes/BasicScene.cs
@@ -1,8 +1,11 @@
+using OpenJam2020;
+
 [Scene("Basic Scene", 0)]
 public class BasicScene : Scene
 {
     public override void Initialize()
     {
         base.Initialize();
+        CreateEntity("player").AddComponent<Player>();
     }
 }
