324f4cb13de3f823b084610922e4492fbd91e9b1
Rouli <nittram36@gmail.com>
2020-09-19 21:13:05 +0200

    Add menu textures and RouliFont

--- Components/CameraFollow.cs
+++ Components/CameraFollow.cs
@@ -20,8 +20,12 @@ public class CameraFollow : Component, IUpdatable
 
     public void Update()
     {
-        offset = Vector2.Zero; // offset = (- 0.5f * (player.Velocity.Y + player.Entity.Position.Y > 0 ? new Vector2(player.Velocity.X, player.Entity.Position.Y) : player.Velocity)) *  0.3f + previous_offset * 0.8f;
-        Vector2 newPosition = player.Transform.Position - offset;
+
+        offset = player.Velocity;
+        if (offset.Length() > 5) offset.Normalize(); 
+        else offset = Vector2.Zero;
+        offset *= -300; offset.X *= 2; if (player.Velocity.Length() < 1000) offset.X *= (player.Velocity.Length())/1000;
+        Vector2 newPosition = player.Transform.Position - offset * 0.5f - previous_offset * 0.5f;
 
         float length = player.Velocity.Length();
         float normalized = Mathf.Clamp01(length / 3000f);
