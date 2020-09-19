d68019b20fd2aac949a1d96af614f2d5e371ee4d
Efflam <efflam-fifi@hotmail.fr>
2020-09-19 12:54:31 +0200

    Add all player states

---
+++ PlayerStates/Throwing_3State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nez.AI.FSM;
+
+public class Throwing_3State : State<Player>
+{
+    public override void Update(float deltaTime)
+    {
+
+    }
+}
