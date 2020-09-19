a23937228143fc898813308ca70adf21cf7f9712
Corentin Girard <corentingirard.dev@gmail.com>
2020-09-19 15:55:28 +0200

    Add trees background

--- Components/Landscape.cs
+++ Components/Landscape.cs
@@ -5,6 +5,9 @@ public class Landscape : Component
 {
     Layer[] layers;
 
+    const int SKY_HEIGHT = Constants.DESIGN_HEIGHT * 50;
+    const int TREES_HEIGHT = Constants.DESIGN_HEIGHT * 10;
+
     struct Layer
     {
         public Transform transform;
@@ -21,15 +24,40 @@ public class Landscape : Component
     {
         base.OnAddedToEntity();
 
-        var sky = Entity.Scene.CreateEntity("sky");
-        sky.Parent = Transform;
-        sky.Transform.Position = new Vector2(0, 300);
+        var sky = AddSky();
+        var trees = AddTrees();
 
-        var renderer = sky.AddTiledTexture("textures/sky", Constants.DESIGN_WIDTH * 100, Constants.DESIGN_HEIGHT * 50);
+        layers = new[] {
+            new Layer(sky.Transform, .7f),
+            new Layer(trees.Transform, .8f)
+        };
+    }
+
+    private Entity AddSky()
+    {
+        var entity = Entity.Scene.CreateEntity("sky");
+        entity.Parent = Transform;
+        entity.Transform.Position = new Vector2(0, 300);
+
+        var renderer = entity.AddTiledTexture("textures/sky", 1, Constants.DESIGN_WIDTH * 100, SKY_HEIGHT);
         renderer.OriginNormalized = new Vector2(.5f, 1);
-        renderer.LayerDepth = 10;
+        return entity;
+    }
 
-        layers = new[] { new Layer(sky.Transform, .8f) };
+    private Entity AddTrees()
+    {
+        var entity = Entity.Scene.CreateEntity("trees");
+        entity.Parent = Transform;
+        entity.Transform.Position = new Vector2(0, 300);
+
+        var treesRenderer = entity.AddTiledTexture("textures/bg1", .9f, Constants.DESIGN_WIDTH * 100, TREES_HEIGHT);
+        treesRenderer.OriginNormalized = new Vector2(.5f, 1);
+
+        var topRenderer = entity.AddTiledTexture("textures/bg1-top", .8f, Constants.DESIGN_WIDTH * 100);
+        topRenderer.OriginNormalized = new Vector2(.5f, 1);
+        topRenderer.LocalOffset = new Vector2(0, -treesRenderer.Origin.Y);
+
+        return entity;
     }
 
     public void Scroll(Vector2 delta)
--- Extensions/EntityExtensions.cs
+++ Extensions/EntityExtensions.cs
@@ -4,18 +4,19 @@ using Nez;
 
 public static class EntityExtensions
 {
-    public static TiledSpriteRenderer AddTiledTexture(this Entity entity, string textureName, int width)
+    public static TiledSpriteRenderer AddTiledTexture(this Entity entity, string textureName, float depth, int width)
     {
-        return entity.AddTiledTexture(textureName, width, null);
+        return entity.AddTiledTexture(textureName, depth, width, null);
     }
 
-    public static TiledSpriteRenderer AddTiledTexture(this Entity entity, string textureName, int width, int? height)
+    public static TiledSpriteRenderer AddTiledTexture(this Entity entity, string textureName, float depth, int width, int? height)
     {
         var texture = entity.Scene.Content.Load<Texture2D>(textureName);
         var renderer = entity.AddComponent(new TiledSpriteRenderer(texture));
         renderer.Width = width;
         if (height.HasValue) renderer.Height = height.Value;
         renderer.OriginNormalized = new Vector2(.5f, 0);
+        renderer.LayerDepth = depth;
         return renderer;
     }
 }
