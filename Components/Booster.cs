e854d3468446e9da43f1af03c320ac813da81654
Efflam <efflam-fifi@hotmail.fr>
2020-09-20 18:07:27 +0200

    Add Booster + Improve Pterodactylus

---
+++ Components/Booster.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.Sprites;
+using System;
+
+class Booster : Pterodactylus
+{
+    public override void OnAddedToEntity()
+    {
+        base.OnAddedToEntity();
+        Entity.GetComponent<BoxCollider>().SetLocalOffset(new Vector2(0, 0));
+
+        Entity.RemoveComponent<SpriteAnimator>();
+        animator = Entity.AddComponent(new SpriteAnimator() { LayerDepth = .5f });
+        animator.AddAnimation("idle", Game.Atlas.GetAnimation("booster"));
+        animator.Play("idle");
+    }
+}
--- Components/Pterodactylus.cs
+++ Components/Pterodactylus.cs
@@ -1,14 +1,17 @@
 ﻿using Nez;
 using Nez.Sprites;
 
-class Pterodactylus : WorldObject, ITriggerListener
+class Pterodactylus : WorldObject, ITriggerListener, IUpdatable
 {
     protected SpriteAnimator animator;
+    protected bool isTriggeredByPlayer;
 
     public override void OnAddedToEntity()
     {
         var collider = Entity.AddComponent(new BoxCollider(-75, -75, 250, 150));
         collider.IsTrigger = true;
+
+        isTriggeredByPlayer = false;
 
         animator = Entity.AddComponent(new SpriteAnimator() { LayerDepth = .5f });
         animator.AddAnimation("idle", Game.Atlas.GetAnimation("ptero"));
@@ -20,11 +23,26 @@ class Pterodactylus : WorldObject, ITriggerListener
     public void OnTriggerEnter(Collider other, Collider local)
     {
         var player = other.GetComponent<Player>();
-        if (player.Velocity.X < 2000)
-            player.Velocity.X += 300;
+        if (player != null)
+        {
+            player.Velocity.Y = 0;
+            isTriggeredByPlayer = true;
+        }
     }
 
     public void OnTriggerExit(Collider other, Collider local)
     {
+        var player = other.GetComponent<Player>();
+        if (player != null)
+            isTriggeredByPlayer = false;
+    }
+
+    public void Update()
+    {
+        if (isTriggeredByPlayer && Game.player.Velocity.X < 2000)
+        {
+            Game.player.Velocity.X += 300 * Time.DeltaTime * Constants.speedMultiplier;
+             
+        }
     }
 }
