4e63cfc68adbc56a2733c6edd6e28e02bc9dfc00
Corentin Girard <corentingirard.dev@gmail.com>
2020-09-20 12:21:09 +0200

    Renamed trees to forest

--- Components/WorldGenerator.cs
+++ Components/WorldGenerator.cs
@@ -68,7 +68,7 @@ class WorldGenerator : Component, IUpdatable
         if (Random.NextInt((int)(500 / probaMultiplier)) == 0)
         {
             int type = Random.NextInt(2);
-            int yPosition = -Constants.TREES_HEIGHT - Random.NextInt(Constants.SKY_HEIGHT - Constants.TREES_HEIGHT);
+            int yPosition = -Constants.FOREST_HEIGHT - Random.NextInt(Constants.SKY_HEIGHT - Constants.FOREST_HEIGHT);
             switch (type)
             {
                 case 0:
