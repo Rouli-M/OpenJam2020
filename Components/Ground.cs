d52e973fb6337c137b7c11b30696a451fab32e3b
Rouli <nittram36@gmail.com>
2020-09-21 14:35:54 +0200

    Fix ground width

--- Components/Ground.cs
+++ Components/Ground.cs
@@ -6,7 +6,7 @@ using static Constants;
 public class Ground : Component
 {
     const int GROUND_HEIGHT = 100;
-    int groundWidth = DESIGN_WIDTH * 100;
+    int groundWidth = DESIGN_WIDTH * 19000;
 
     public override void OnAddedToEntity()
     {
